21ee018cc33bce2738ba947921e97b8c83e47884
DeFiXiK <mr.defix@bk.ru>
2016-02-01 14:00:33 +0500

    First source

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SomeFirstPage(c *gin.Context) {
+	c.String(http.StatusOK, "Ну здраствуй,\n")
+	c.String(http.StatusOK, "Ты на заглавной странице сайта,\n")
+	c.String(http.StatusOK, "На котором пока что совсем ничего нету")
+}
+
+func main() {
+	site := gin.Default()
+	site.GET("/", SomeFirstPage)
+	site.Run(":8080")
+}
