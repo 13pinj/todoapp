6503798d0914f6c245a80f9a1f58c5ef5653e45c
Oleg Yamnikov <bgeyts667@gmail.com>
2016-02-03 20:31:06 +0500

    First party of TodoList tests

---
+++ models/todo_list/todo_list.go
@@ -0,0 +1,68 @@
+package todo_list
+
+import "github.com/13pinj/todoapp/models/todo"
+
+type TodoList struct {
+	ID    uint
+	Title string
+	Todos []*todo.Todo
+}
+
+// New создает новый экземпляр TodoList, но не сохраняет его.
+// Сохранение должно производиться с помощью функции TodoList.Save().
+func New(t string) *TodoList {
+	return nil
+}
+
+// Find возвращает TodoList, сохраненный в базе и имеющий заданный id.
+// В случае если TodoList не был найден, Find вернет вторым значением false.
+// В случае успеха, второе возвращаемое значение будет true.
+func Find(id uint) (*TodoList, bool) {
+	return nil, false
+}
+
+// Save сохраняет структуру в базу. Если структура не была прежде сохранена,
+// она будет добавлена в базу, в противном случае, ее представление в базе
+// будет обновлено.
+// Save возвращает ненулевую ошибку в случае невалидности одного их полей структуры.
+// В этом случае сохранение не будет произведено.
+func (l *TodoList) Save() error {
+	return nil
+}
+
+// Destroy удаляет структуры из базы.
+func (l *TodoList) Destroy() {
+
+}
+
+// Len возвращает количество всех дел в списке.
+func (l *TodoList) Len() int {
+	return 0
+}
+
+// LenUndone возвращает количество незавершенных дел в списке.
+func (l *TodoList) LenUndone() int {
+	return 0
+}
+
+// LenDone возвращает количество завершенных дел в списке.
+func (l *TodoList) LenDone() int {
+	return 0
+}
+
+// Add добавляет в список новое назавершенное дело c текстом lbl и сохраняет его в базу.
+// Add не позволяет добавлять дела в несохраненную в базу списки, о чем сообщает ошибкой.
+// Add также возвращает ошибки сохранения Todo.
+func (l *TodoList) Add(lbl string) error {
+	return nil
+}
+
+// Undone возвращает список незавершенных дел.
+func (l *TodoList) Undone() []*TodoList {
+	return nil
+}
+
+// Done возвращает список завершенных дел.
+func (l *TodoList) Done() []*TodoList {
+	return nil
+}
---
+++ models/todo_list/todo_list_test.go
@@ -0,0 +1,238 @@
+package todo_list
+
+import "testing"
+
+func TestNew(t *testing.T) {
+	title1 := "First todolist"
+	title2 := "Second todolist"
+
+	list1 := New(title1)
+	list2 := New(title2)
+	empty := New("")
+
+	if list1 == nil || list2 == nil || empty == nil {
+		t.Fatal("New() не должно возвращать nil")
+	}
+
+	if list1.ID != 0 || list2.ID != 0 || empty.ID != 0 {
+		t.Error("ID несохраненных списков не должны быть присвоены")
+	}
+
+	if list1.Title != title1 || list2.Title != title2 || empty.Title != "" {
+		t.Error("New() должно сохранять заголовок списка")
+	}
+}
+
+func TestTodoList_Save(t *testing.T) {
+	title1 := "First todolist"
+	title2 := "Second todolist"
+
+	list1 := New(title1)
+	list2 := New(title2)
+	empty := New("")
+
+	if list1 == nil || list2 == nil || empty == nil {
+		t.FailNow()
+	}
+
+	if err := list1.Save(); err != nil {
+		t.Error("Сохранение валидного списка не должно вызывать ошибок")
+	}
+	if err := list2.Save(); err != nil {
+		t.Error("Сохранение валидного списка не должно вызывать ошибок")
+	}
+	if err := empty.Save(); err == nil {
+		t.Error("Сохранение списка с пустым текстом не должно быть допущено")
+	}
+
+	if list1.ID == 0 || list2.ID == 0 {
+		t.Error("После сохранения Todo должен быть присвоен ID")
+	}
+	if empty.ID != 0 {
+		t.Error("Сохранение с ошибкой не должно изменять ID структуры")
+	}
+
+	if t.Failed() {
+		t.FailNow()
+	}
+
+	list1_0, ok := Find(list1.ID)
+	if !ok {
+		t.Fatal("Успешно сохраненный список должен быть найдено по ID")
+	}
+	if list1_0 == nil {
+		t.FailNow()
+	}
+
+	list2_0, ok := Find(list2.ID)
+	if !ok {
+		t.Fatal("Успешно сохраненный список должен быть найдено по ID")
+	}
+	if list2_0 == nil {
+		t.FailNow()
+	}
+
+	if list1_0.Title != title1 || list2_0.Title != title2 {
+		t.Error("Поле Title должно успешно сохраняться")
+	}
+
+	if t.Failed() {
+		t.FailNow()
+	}
+
+	list2_0.Title = ""
+	if err := list2_0.Save(); err == nil {
+		t.Error("Сохранение созданного списка с пустым заголовком не должно быть допущено")
+	}
+	list2_1, _ := Find(list2_0.ID)
+	if list2_1.Title != title2 {
+		t.Error("В случае безуспешного сохранения, структура в базе не должна быть изменена")
+	}
+
+	list2_1.Title = "Changed text"
+	list2_2, _ := Find(list2_1.ID)
+	if list2_2.Title != title2 {
+		t.Error("В случае несохранения, структура в базе не должна быть изменена")
+	}
+}
+
+func TestFind(t *testing.T) {
+	title1 := "First todolist"
+	title2 := "Second todolist"
+
+	list1 := New(title1)
+	list2 := New(title2)
+	empty := New("")
+
+	if list1 == nil || list2 == nil || empty == nil {
+		t.FailNow()
+	}
+	if err := list1.Save(); err != nil {
+		t.FailNow()
+	}
+	if err := list2.Save(); err != nil {
+		t.FailNow()
+	}
+
+	list1_0, ok := Find(list1.ID)
+	if !ok {
+		t.Fatal("Find() должно находить сохраненные структуры")
+	}
+	if list1_0 == nil {
+		t.Fatal("Find() не должно возвращать nil в случае успешной находки")
+	}
+	if list1_0.ID != list1.ID || list1_0.Title != list1.Title {
+		t.Fatal("Todo, найденного через Find() должно быть эквивалентно искомому")
+	}
+
+	list2_0, ok := Find(list2.ID)
+	if !ok {
+		t.Fatal("Find() должно находить сохраненные структуры")
+	}
+	if list2_0 == nil {
+		t.Fatal("Find() не должно возвращать nil в случае успешной находки")
+	}
+	if list2_0.ID != list2.ID || list2_0.Title != list2.Title {
+		t.Fatal("Todo, найденного через Find() должно быть эквивалентно искомому")
+	}
+
+	randomID := uint(1337)
+	undef, ok := Find(randomID)
+	if undef != nil || ok {
+		t.Error("Find() должно отвечать nil и false при поиске несуществующих структур")
+	}
+}
+
+func TestTodoList_Destroy(t *testing.T) {
+	title1 := "First todolist"
+	title2 := "Second todolist"
+
+	list1 := New(title1)
+	list2 := New(title2)
+	unsaved := New("Unsaved")
+
+	if list1 == nil || list2 == nil || unsaved == nil {
+		t.FailNow()
+	}
+	if err := list1.Save(); err != nil {
+		t.FailNow()
+	}
+	if err := list2.Save(); err != nil {
+		t.FailNow()
+	}
+
+	id1 := list1.ID
+	id2 := list2.ID
+	list1.Destroy()
+	if _, ok := Find(id1); ok {
+		t.Error("Структура уничтоженная с помощью Destroy(), не должна быть найдена в базе")
+	}
+
+	if _, ok := Find(id2); !ok {
+		t.Error("Destroy() не должно затрагивать другие структуры в базе и их ID")
+	}
+
+	if t.Failed() {
+		t.FailNow()
+	}
+
+	t.Log("Destroy() должно без паники обрабатывать несохраненные структуры")
+	unsaved.Destroy()
+
+	if _, ok := Find(id1); ok {
+		t.Error("Destroy() на несохраненной структуре не должно затрагивать другие структуры в базе")
+	}
+	if _, ok := Find(id2); !ok {
+		t.Error("Destroy() на несохраненной структуре не должно затрагивать другие структуры в базе")
+	}
+}
+
+func TestTodoList_LenDoneUndone(t *testing.T) {
+	title1 := "First todolist"
+	title2 := "Second todolist"
+
+	list1 := New(title1)
+	list2 := New(title2)
+
+	if list1 == nil || list2 == nil {
+		t.FailNow()
+	}
+	if err := list1.Save(); err != nil {
+		t.FailNow()
+	}
+	if err := list2.Save(); err != nil {
+		t.FailNow()
+	}
+
+	list1.Add("Task 1")
+	list1.Todos[0].Done = true
+	list1.Todos[0].Save()
+	list1.Add("Task 2")
+	list1.Add("Task 3")
+
+	list2.Add("Task 1")
+	list2.Add("Task 2")
+	list2.Todos[1].Done = true
+	list2.Todos[1].Save()
+
+	if list1.Len() != 3 {
+		t.Errorf("Len() должно возвращать длину списка. Ожидалось %v, получено %v.", 3, list1.Len())
+	}
+	if list2.Len() != 2 {
+		t.Errorf("Len() должно возвращать длину списка. Ожидалось %v, получено %v.", 2, list2.Len())
+	}
+
+	if list1.LenDone() != 1 {
+		t.Errorf("LenDone() должно возвращать длину списка. Ожидалось %v, получено %v.", 1, list1.LenDone())
+	}
+	if list2.LenDone() != 1 {
+		t.Errorf("LenDone() должно возвращать длину списка. Ожидалось %v, получено %v.", 1, list2.LenDone())
+	}
+
+	if list1.LenUndone() != 2 {
+		t.Errorf("LenUndone() должно возвращать длину списка. Ожидалось %v, получено %v.", 2, list1.LenUndone())
+	}
+	if list2.LenUndone() != 1 {
+		t.Errorf("LenUndone() должно возвращать длину списка. Ожидалось %v, получено %v.", 1, list2.LenUndone())
+	}
+}
