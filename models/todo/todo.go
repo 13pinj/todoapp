7befb2b324877c3e2fd5fc1172d5545bdd021f03
DeFiXiK <mr.defix@bk.ru>
2016-02-03 19:32:25 +0500

    add todo

--- models/todo/todo.go
+++ models/todo/todo.go
@@ -1,5 +1,7 @@
 package todo
 
+import "errors"
+
 // Todo - это cтруктура одного "дела".
 type Todo struct {
 	ID uint
@@ -9,16 +11,34 @@ type Todo struct {
 	Label string
 }
 
+var st = make(map[uint]*Todo)
+var key uint
+
 // New создает новый экземпляр Todo, но не сохраняет его.
 // Сохранение должно производиться с помощью функции Todo.Save().
 func New(l string) *Todo {
-	return nil
+	record := Todo{}
+	record.Done = false
+	record.Label = l
+	return &record
+}
+
+func stCopy(t *Todo) *Todo {
+	return &Todo{
+		ID:    t.ID,
+		Done:  t.Done,
+		Label: t.Label,
+	}
 }
 
 // Find возвращает Todo, сохраненный в базе и имеющий заданный id.
 // В случае если Todo не был найден, Find вернет вторым значением false.
 // В случае успеха, второе возвращаемое значение будет true.
 func Find(id uint) (*Todo, bool) {
+	record, ok := st[id]
+	if ok {
+		return stCopy(record), true
+	}
 	return nil, false
 }
 
@@ -28,10 +48,21 @@ func Find(id uint) (*Todo, bool) {
 // Save возвращает ненулевую ошибку в случае невалидности одного их полей структуры.
 // В этом случае сохранение не будет произведено.
 func (t *Todo) Save() error {
+	if t.Label == "" {
+		return errors.New("Текст задания не должен быть пустым")
+	}
+	if t.ID == 0 {
+		key++
+		t.ID = key
+		st[t.ID] = stCopy(t)
+	} else {
+		st[t.ID].Done = t.Done
+		st[t.ID].Label = t.Label
+	}
 	return nil
 }
 
 // Destroy удаляет структуры из базы.
 func (t *Todo) Destroy() {
-
+	delete(st, t.ID)
 }
