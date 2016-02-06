650b0157c0ea88effd9d1a080000400ae33297e4
Oleg Yamnikov <bgeyts667@gmail.com>
2016-02-06 15:08:11 +0500

    Better ctl docs

--- controllers/ctl.go
+++ controllers/ctl.go
@@ -2,27 +2,27 @@ package ctl
 
 import "github.com/gin-gonic/gin"
 
-// Отвечает на запрос кодом 200 и выполненным шаблоном.
+// RenderHTML отвечает на запрос кодом 200 и выполненным шаблоном.
 func RenderHTML(c *gin.Context, template string, data interface{}) {
 
 }
 
-// Отвечает на запрос кодом 200 и данными data, закодированными в JSON.
+// RenderJSON отвечает на запрос кодом 200 и данными data, закодированными в JSON.
 func RenderJSON(c *gin.Context, data interface{}) {
 
 }
 
-// Отвечает кодом 403 Forbidden.
+// Render403 отвечает кодом 403 Forbidden.
 func Render403(c *gin.Context) {
 
 }
 
-// Отвечает кодом 404 Not Found.
+// Render404 отвечает кодом 404 Not Found.
 func Render404(c *gin.Context) {
 
 }
 
-// Отвечает кодом 500 Internal Server Error.
+// Render500 отвечает кодом 500 Internal Server Error.
 func Render500(c *gin.Context) {
 
 }
