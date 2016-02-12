130bc4faef94cfa3ed3d4d49977ee5f5c8487598
DeFiXiK <mr.defix@bk.ru>
2016-02-12 20:45:53 +0500

    DB support

--- models/models.go
+++ models/models.go
@@ -1,1 +1,20 @@
 package models
+
+import (
+	"github.com/13pinj/todoapp/core/log"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var DB gorm.DB
+
+func init() {
+	db, err := gorm.Open("sqlite3", "debug.db")
+	if err != nil {
+		panic(err)
+	}
+	DB = db
+	DB.LogMode(true)
+	DB.SetLogger(gorm.Logger{LogWriter: log.Logger()})
+}
