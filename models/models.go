a004caa42cd1cd86d906290e423d884dcb3c70a6
Oleg Yamnikov <bgeyts667@gmail.com>
2016-02-14 18:21:47 +0500

    Connect to postresql on heroku

--- models/models.go
+++ models/models.go
@@ -1,22 +1,31 @@
 package models
 
 import (
+	"os"
+
 	"github.com/13pinj/todoapp/core/log"
 
 	"github.com/13pinj/todoapp/Godeps/_workspace/src/github.com/jinzhu/gorm"
 	// SQLite driver for gorm
 	_ "github.com/13pinj/todoapp/Godeps/_workspace/src/github.com/mattn/go-sqlite3"
+	// PostreSQL driver for gorm
+	_ "github.com/13pinj/todoapp/Godeps/_workspace/src/github.com/lib/pq"
 )
 
 // DB - переменная для доступа к БД
 var DB gorm.DB
 
 func init() {
-	db, err := gorm.Open("sqlite3", "debug.db")
+	mode := os.Getenv("TODOAPP_MODE")
+	var err error
+	if mode == "HEROKU" {
+		DB, err = gorm.Open("postgres", os.Getenv("DATABASE_URL"))
+	} else {
+		DB, err = gorm.Open("sqlite3", "debug.db")
+	}
 	if err != nil {
 		panic(err)
 	}
-	DB = db
 	DB.LogMode(true)
 	DB.SetLogger(gorm.Logger{LogWriter: log.Logger()})
 }
