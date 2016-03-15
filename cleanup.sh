ed61592fcab6a103208989c7ae9bb10b4ceaed23
Oleg Yamnikov <bgeyts667@gmail.com>
2016-03-15 18:13:48 +0500

    Better cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-rm debug.db server.log
-rm controllers/users/debug.db controllers/users/server.log
-rm models/session/debug.db models/session/server.log
-rm models/todo/debug.db models/todo/server.log
-rm models/todolist/debug.db models/todolist/server.log
-rm models/user/debug.db models/user/server.log
+find . -name server.log -delete
+find . -name debug.db -delete
