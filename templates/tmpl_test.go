aa2fdcf362304e9a2a1607ebca8b06b6b5931ae8
Oleg Yamnikov <bgeyts667@gmail.com>
2016-03-19 22:59:12 +0500

    Fix tmpl.ago test

--- templates/tmpl_test.go
+++ templates/tmpl_test.go
@@ -16,7 +16,7 @@ func TestAgo(t *testing.T) {
 		{now.Add(-59 * time.Minute), "59 минут назад"},
 		{now.Add(-1 * time.Hour), "1 час назад"},
 		{now.Add(-2 * time.Hour), "2 часа назад"},
-		{now.Add(-time.Duration(now.Hour()) * time.Hour), "вчера"},
+		{now.Add(-time.Duration(now.Hour()+1) * time.Hour), "вчера"},
 		{now.AddDate(0, 0, -1), "вчера"},
 		{now.AddDate(0, 0, -2), "позавчера"},
 		{time.Date(2000, time.March, 4, 12, 8, 0, 0, time.Local), "04.03.2000"},
