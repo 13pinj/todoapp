e6dcb3814729890b4cb24cc392b3cd98e67504c4
Oleg Yamnikov <bgeyts667@gmail.com>
2016-03-19 22:37:19 +0500

    Show "last visit ago" string when user is offline

--- templates/tmpl.go
+++ templates/tmpl.go
@@ -1,6 +1,10 @@
 package tmpl
 
-import "html/template"
+import (
+	"fmt"
+	"html/template"
+	"time"
+)
 
 var fm = template.FuncMap{
 	// pages создает массив номеров страниц, которые следует вывести
@@ -19,20 +23,47 @@ var fm = template.FuncMap{
 	// one: (один) мяч
 	// few: (два) мяча
 	// many: (много) мячей
-	"rplural": func(count int, one, few, many string) string {
-		d := count % 10
-		dd := count % 100
-		switch {
-		case dd >= 11 && dd <= 20:
-			return many
-		case d == 1:
-			return one
-		case d >= 2 && d <= 4:
-			return few
-		default:
-			return many
-		}
-	},
+	"rplural": rplural,
+	// ago формирует строку типа "X часов назад" из обозначенной точки во времени.
+	"ago": ago,
+}
+
+// rplural доступна в шаблонах через fm["rplural"]
+func rplural(count int, one, few, many string) string {
+	d := count % 10
+	dd := count % 100
+	switch {
+	case dd >= 11 && dd <= 20:
+		return many
+	case d == 1:
+		return one
+	case d >= 2 && d <= 4:
+		return few
+	default:
+		return many
+	}
+}
+
+// ago доступна в шаблонах через fm["ago"]
+func ago(t time.Time) string {
+	now := time.Now()
+	diff := now.Sub(t)
+	hours := int(diff.Hours())
+	days := int(now.Truncate(24*time.Hour).Sub(t.Truncate(24*time.Hour)).Hours() / 24)
+	switch {
+	case hours == 0:
+		m := diff.Minutes()
+		return fmt.Sprintf("%d %s назад", int(m), rplural(int(m), "минуту", "минуты", "минут"))
+	case days == 0 && hours < 24:
+		h := diff.Hours()
+		return fmt.Sprintf("%d %s назад", int(h), rplural(int(h), "час", "часа", "часов"))
+	case days == 1:
+		return "вчера"
+	case days == 2:
+		return "позавчера"
+	default:
+		return t.Format("02.01.2006")
+	}
 }
 
 func Funcs() template.FuncMap {
---
+++ templates/tmpl_test.go
@@ -0,0 +1,31 @@
+package tmpl
+
+import (
+	"testing"
+	"time"
+)
+
+func TestAgo(t *testing.T) {
+	now := time.Now()
+	cases := []struct {
+		t   time.Time
+		res string
+	}{
+		{now.Add(-1 * time.Minute), "1 минуту назад"},
+		{now.Add(-2 * time.Minute), "2 минуты назад"},
+		{now.Add(-59 * time.Minute), "59 минут назад"},
+		{now.Add(-1 * time.Hour), "1 час назад"},
+		{now.Add(-2 * time.Hour), "2 часа назад"},
+		{now.Add(-time.Duration(now.Hour()) * time.Hour), "вчера"},
+		{now.AddDate(0, 0, -1), "вчера"},
+		{now.AddDate(0, 0, -2), "позавчера"},
+		{time.Date(2000, time.March, 4, 12, 8, 0, 0, time.Local), "04.03.2000"},
+	}
+
+	for _, c := range cases {
+		res := ago(c.t)
+		if res != c.res {
+			t.Errorf("ago(%v): получено %v, ожидалось %v", c.t, res, c.res)
+		}
+	}
+}
